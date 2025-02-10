047cbea2e3ec10cd10b3c31f1998b76554d628d4
Rtopdar <rounak2002topdar@gmail.com>
2025-02-10 16:42:45 +0530

    Add import statement for views in urls.py

--- monthly_challenge/urls.py
+++ monthly_challenge/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponseRedirect
+from . import views
 
 urlpatterns = [
     path("", lambda request: HttpResponseRedirect("/challenges/")),
